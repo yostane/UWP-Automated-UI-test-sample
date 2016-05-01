17edcfeb55650c526b7bb558a4ce883b09044870
yostane <yostane@yahoo.fr>
2016-05-01 21:27:23 +0200

    commit of test app and main app

---
+++ UI-test-app/BasicScenarios.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium.iOS;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class BasicScenarios
+    {
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        protected static IOSDriver<IOSElement> AppSession;
+        protected static IOSElement RestultTextElement;
+        protected static IOSElement ButtonPlusElement;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            //Launch the app
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", "57b3a460-8843-4d84-822a-9f316274c2bf_tz6ph9wdjhqw8!App");
+            AppSession = new IOSDriver<IOSElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+            Assert.IsNotNull(AppSession);
+            AppSession.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+
+            //locate elements
+            ButtonPlusElement = AppSession.FindElementByAccessibilityId("btn_plus") as IOSElement;
+            Assert.IsNotNull(ButtonPlusElement);
+            ButtonPlusElement.Click();
+            ButtonPlusElement.Click();
+            RestultTextElement = AppSession.FindElementByAccessibilityId("result") as IOSElement;
+            Assert.IsNotNull(RestultTextElement);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            RestultTextElement = null;
+            AppSession.Dispose();
+            AppSession = null;
+        }
+
+        [TestMethod]
+        public void Case1()
+        {
+            Assert.AreEqual("2", RestultTextElement.Text);
+        }
+
+        [TestMethod]
+        public void Case2()
+        {
+            ButtonPlusElement.Click();
+            Assert.AreEqual("2", RestultTextElement.Text);
+        }
+    }
+}
---
+++ main-app/main-app/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace mainapp
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public int currentValue { get; set; }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            currentValue = currentValue + 1;
+            textBlock.Text = "" + currentValue;
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        
+    }
+}
